ebd104ea43a51155892128a682446cfcbd14c183
DavidYauri1 <david.yauri@tecsup.edu.pe>
2022-11-04 11:27:39 -0500

    test branch

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-console.log("Test");
+/*
+Ejercicio I
+Construye un programa que dado el peso en (kg) y la altura de una persona en (metros) calcule y 
+muestre por pantalla su Índice de Masa Corporal (IMS) o índice de Quetelet. Este índice pretende determinar el 
+intervalo de peso más saludable que puede tener una persona. El valor de este índice se calcula mediante la siguiente expresión:
+
+Se suele establecer un intervalo de 18 a 25 como saludable en personas adultas,
+ de lo contrario se deberá especificar que la persona tiene sobre peso.
+
+
+*/
+
+// let Peso =  parseInt(prompt("Ingrese su Peso en KG"));
+// let Altura = parseFloat(prompt("Ingrese su Altura en Metros"));
+// let masaCorporal = Math.round(Peso/(Altura*Altura)) ;
+// alert("Su Indice de masa corporal es "+masaCorporal);
+
+// if (masaCorporal >= 18 && masaCorporal <=25) {
+//     alert("Persona saludable")
+// } else {
+//     alert("SobrePeso")
+// }
+
+
+
+/*
+Realizar un programa para una tienda de coches: Si el coche a la venta es un Ford fiesta,
+ el descuento es de un 5%. Si el coche a la venta es un ford focus, el descuento es del 10%.
+ El usuario introduce el artículo (o su código) y el programa saca el descuento correspondiente por pantalla.
+
+*/
+// let marca,modelo,descuento = 0;
+
+//   marca =  prompt("Marca");
+//   modelo = prompt("Modelo");
+//   let a = marca.toLowerCase();
+//   let b = modelo.toLowerCase();
+ 
+//  if ((a=="ford") && (b =="fiesta"))
+//  {
+//  descuento = 5;
+//  }
+
+// if ((a=="ford") && (b =="focus"))
+//  {
+//  descuento = 10;
+//  } 
+
+//  console.log(marca);
+
+// alert("Su descuento es "+descuento)
+
